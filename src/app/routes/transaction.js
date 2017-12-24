b5801c59423a7312779348a004717ac5400882cb
viktornord1 <viktornord1@gmail.com>
2017-12-24 16:30:40 +0200

    add validation for amount for transactions

--- src/app/routes/transaction.js
+++ src/app/routes/transaction.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const Transaction = require('../db/models/transaction');
 const User = require('../db/models/user');
 const authMiddleWare = require('../auth/auth-middleware');
@@ -10,15 +11,23 @@ router.get('/transactions', authMiddleWare, transactionListGET);
 module.exports = router;
 
 async function transactionPOST(req, res, next) {
-  const {amount, to} = req.body;
-  const transaction = await new Transaction({
-    from: req.userData.account,
-    amount,
-    to
-  }).save();
-  await User.updateBalance(req.userData._id, -transaction.amount);
+  try {
+    const {amount, to} = req.body;
+    if (amount > 0) {
+      const transaction = await new Transaction({
+        from: req.userData.account,
+        amount,
+        to
+      }).save();
+      await User.updateBalance(req.userData._id, -transaction.amount);
 
-  res.send(transaction);
+      res.send(transaction);
+    } else {
+      next(createError(400, 'Not valid amount'));
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 async function transactionListGET(req, res, next) {
   try {
