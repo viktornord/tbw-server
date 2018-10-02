544172c8e8f352fe9bd2844ece93577e2973f681
Viktor <v.urbanas@mozidev.com>
2018-10-02 12:38:46 +0300

    change start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/home/ubuntu/my-app/node_modules/.bin/pm2 start /home/ubuntu/my-app/index.js --name "my-app" --force &
+./node_modules/.bin/pm2 start ./index.js --name "my-app" --force
