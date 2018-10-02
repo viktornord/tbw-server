931fafb1f2c1fa167326a1e097ac950e69b414f3
Viktor <v.urbanas@mozidev.com>
2018-10-02 13:15:10 +0300

    change start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH/..
-./node_modules/.bin/pm2 start ./index.js --name "my-app" --force
+nohup ./node_modules/.bin/pm2 start ./index.js --name "my-app" --force
